5a2cb2d5ac12a1fe6834800084da077496f666ef
Denis Moiseev <isdenmois@gmail.com>
2021-01-31 15:54:00 +0300

    fix(config): parse data

--- src/config.js
+++ src/config.js
@@ -6,9 +6,15 @@ function getEnv(param, defaultValue) {
   return process.env[param] || defaultValue;
 }
 
+function getInt(param, defaultValue) {
+  return Number(getEnv(param)) || defaultValue;
+}
+
 export const config = {
   PORT: getEnv('PORT', 3000),
-  SCRAPPER_TIMEOUT: getEnv('SCRAPPER_TIMEOUT', 120),
+  SCRAPPER_TIMEOUT: getInt('SCRAPPER_TIMEOUT', 5),
   ETH_API_URL: getEnv('ETH_API_URL'),
   BTC_API_URL: getEnv('BTC_API_URL'),
 };
+
+console.log(config);
--- src/scrapper/page-gen.js
+++ src/scrapper/page-gen.js
@@ -4,7 +4,7 @@ import { config } from '../config.js';
 const MAX = 904625697166532776746648320380374280100293470930272690489102837043110636675n;
 
 let start = 0;
-const TRESHHOLD = config.TIMEOUT * 1000;
+const TRESHHOLD = config.SCRAPPER_TIMEOUT * 1000;
 
 export function nextPage(prevPage) {
   if (Date.now() - start > TRESHHOLD) {
